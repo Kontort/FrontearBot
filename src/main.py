dff90bdc41052d26f2a072c1bf393919e5d2a2f5
Ali Rizvi <frontear4594@gmail.com>
2020-01-18 14:15:19 -0500

    Add the ability to respond
    The test for this was done between me and Kuroda, https://www.reddit.com/r/example/comments/eqkmhr/ufrontearbot_test/fetf6ab?utm_source=share&utm_medium=web2x

--- src/main.py
+++ src/main.py
@@ -1,20 +1,31 @@
 import praw
+from praw.models import Comment
 import logging
 
+DEBUG = False # if you wish to log at the debugging level. This shouldn't ideally be active unless necessary
+
 def main():
-    handler = logging.StreamHandler()
-    handler.setLevel(logging.DEBUG)
-
-    logger = logging.getLogger("prawcore")
-    logger.setLevel(logging.DEBUG)
-    
-    logger.addHandler(handler)
-
     reddit = praw.Reddit("FrontearBot")
     print("Logged in as %s" % reddit.user.me())
+
+    for msg in reddit.inbox.unread():
+        if not isinstance(msg, Comment):
+            continue
+
+        if msg.body == "u/FrontearBot --help" or msg.body == "u/Frontear \\--help": # the backslash is added by the new website format thing. what even...
+            msg.mark_read()
+            msg.reply("This is a valid call!")
 
     return 0
 
 if __name__ == "__main__":
-    ret = main()
-    print("\nExited with code %d" % ret)
+    if DEBUG:
+        handler = logging.StreamHandler()
+        handler.setLevel(logging.DEBUG)
+
+        logger = logging.getLogger("prawcore")
+        logger.setLevel(logging.DEBUG)
+    
+        logger.addHandler(handler)
+
+    print("\nExited with code %d" % main())
